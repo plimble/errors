444e4d23188e5f5acd68c6c780d4d9ba3cac4d6b
Witoo Harianto <witoo@plimble.com>
2017-05-30 22:03:45 +0700

    update

---
+++ http.go
@@ -0,0 +1,169 @@
+package errors
+
+import (
+	"fmt"
+
+	"go.uber.org/zap/zapcore"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var mapGrpcCode = map[int]codes.Code{
+	400: 3,
+	401: 16,
+	403: 7,
+	404: 5,
+	441: 4,
+	500: 13,
+	501: 12,
+	503: 14,
+	520: 2,
+}
+
+type HTTPError struct {
+	status  int
+	Code    string `json:"error"`
+	Message string `json:"error_description"`
+	cause   error
+}
+
+func (e *HTTPError) Error() string { return e.Message }
+func (e *HTTPError) Cause() error  { return e.cause }
+func (e *HTTPError) WithCause(err error) *HTTPError {
+	e.cause = err
+	return e
+}
+
+func (e *HTTPError) MarshalLogObject(enc zapcore.ObjectEncoder) error {
+	enc.AddString("error_code", e.Code)
+	enc.AddString("error_description", e.Message)
+	if e.cause != nil {
+		enc.AddString("error_cause", e.cause.Error())
+	}
+
+	return nil
+}
+
+func Error(status int, code, msg string) *HTTPError {
+	return &HTTPError{status, code, msg, nil}
+}
+
+func Errorf(status int, code, format string, v ...interface{}) *HTTPError {
+	return &HTTPError{status, code, fmt.Sprintf(format, v...), nil}
+}
+
+func BadRequest(code, msg string) *HTTPError {
+	return &HTTPError{400, code, msg, nil}
+}
+
+func BadRequestf(code, format string, v ...interface{}) *HTTPError {
+	return &HTTPError{400, code, fmt.Sprintf(format, v...), nil}
+}
+
+func Unauthorized(code, msg string) *HTTPError {
+	return &HTTPError{401, code, msg, nil}
+}
+
+func Unauthorizedf(code, format string, v ...interface{}) *HTTPError {
+	return &HTTPError{401, code, fmt.Sprintf(format, v...), nil}
+}
+
+func Forbidden(code, msg string) *HTTPError {
+	return &HTTPError{403, code, msg, nil}
+}
+
+func Forbiddenf(code, format string, v ...interface{}) *HTTPError {
+	return &HTTPError{403, code, fmt.Sprintf(format, v...), nil}
+}
+
+func NotFound(code, msg string) *HTTPError {
+	return &HTTPError{400, code, msg, nil}
+}
+
+func NotFoundf(code, format string, v ...interface{}) *HTTPError {
+	return &HTTPError{404, code, fmt.Sprintf(format, v...), nil}
+}
+
+func InternalError(code, msg string) *HTTPError {
+	return &HTTPError{400, code, msg, nil}
+}
+
+func InternalErrorf(code, format string, v ...interface{}) *HTTPError {
+	return &HTTPError{500, code, fmt.Sprintf(format, v...), nil}
+}
+
+func Timeout(code, msg string) *HTTPError {
+	return &HTTPError{441, code, msg, nil}
+}
+
+func Timeoutf(code, format string, v ...interface{}) *HTTPError {
+	return &HTTPError{441, code, fmt.Sprintf(format, v...), nil}
+}
+
+func NotImplement(code, msg string) *HTTPError {
+	return &HTTPError{501, code, msg, nil}
+}
+
+func NotImplementf(code, format string, v ...interface{}) *HTTPError {
+	return &HTTPError{501, code, fmt.Sprintf(format, v...), nil}
+}
+
+func Unavailable(code, msg string) *HTTPError {
+	return &HTTPError{503, code, msg, nil}
+}
+
+func Unavailablef(code, format string, v ...interface{}) *HTTPError {
+	return &HTTPError{503, code, fmt.Sprintf(format, v...), nil}
+}
+
+func UnknownError(code, msg string) *HTTPError {
+	return &HTTPError{520, code, msg, nil}
+}
+
+func UnknownErrorf(code, format string, v ...interface{}) *HTTPError {
+	return &HTTPError{520, code, fmt.Sprintf(format, v...), nil}
+}
+
+func FromError(err error) (*HTTPError, bool) {
+	herr, ok := err.(*HTTPError)
+	return herr, ok
+}
+
+func ToGRPC(err error) error {
+	herr, ok := err.(*HTTPError)
+	if !ok {
+		return status.Error(2, err.Error())
+	}
+
+	c := mapGrpcCode[herr.status]
+	return status.Errorf(c, "error=%s error_description=%s", herr.Code, herr.Message)
+}
+
+func errStatus(err error) int {
+	herr, ok := err.(*HTTPError)
+	if !ok {
+		return 0
+	}
+
+	return herr.status
+}
+
+func IsNotFound(err error) bool {
+	return err != nil && errStatus(err) == 404
+}
+
+func IsInternalError(err error) bool {
+	return err != nil && errStatus(err) == 500
+}
+
+func IsBadRequest(err error) bool {
+	return err != nil && errStatus(err) == 400
+}
+
+func IsUnauthorized(err error) bool {
+	return err != nil && errStatus(err) == 401
+}
+
+func IsForbidden(err error) bool {
+	return err != nil && errStatus(err) == 403
+}
